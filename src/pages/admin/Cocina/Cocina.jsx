c7c574a2d87961b356b93d0786ab0cb8ca935278
Vittorio Adesso <adessovittorio8@gmail.com>
2022-03-24 12:13:21 +0100

    modify pages admin kitchen

--- src/pages/admin/Cocina/Cocina.jsx
+++ src/pages/admin/Cocina/Cocina.jsx
@@ -7,194 +7,230 @@ import KitchenItem from "src/components/kitchenItem/kitchenItem";
 import "./styles.scss";
 
 const Cocina = () => {
-  const [datosArray, setDatosArray] = useState([]);
-  const [comandaDatos, setComandaDatos] = useState([]);
-  const [comanda, setComanda] = useState();
-  const [cocinaDatos, setCocinaDatos] = useState([]);
+    const [datosArray, setDatosArray] = useState([]);
+    const [comandaDatos, setComandaDatos] = useState([]);
+    const [comanda, setComanda] = useState();
+    const [cocinaDatos, setCocinaDatos] = useState([]);
 
-  /* Estados de la cocina */
+    /* Estados de la cocina */
 
-  const [pedidosCocina, setPedidosCocina] = useState([]);
-  const [elaboracionCocina, setElaboracionCocina] = useState([]);
-  const [terminadosCocina, setTerminadosCocina] = useState([]);
-  const [servidoCocina, setServidosCocina] = useState([]);
-  const [ticketCocina, setTicketCocina] = useState([]);
-  
-  const [noSonTicket, setNoSonTicket] = useState([]);
-  const [pedidosArray, setPedidosArray] = useState([]);
-  const [platejos, setPlatejos] = useState([]);
-  const [finalArray, setFinalArray] = useState([]);
+    const [pedidosCocina, setPedidosCocina] = useState([]);
+    const [elaboracionCocina, setElaboracionCocina] = useState([]);
+    const [terminadosCocina, setTerminadosCocina] = useState([]);
+    const [servidoCocina, setServidosCocina] = useState([]);
+    const [ticketCocina, setTicketCocina] = useState([]);
 
+    const [noSonTicket, setNoSonTicket] = useState([]);
+    const [pedidosArray, setPedidosArray] = useState([]);
+    const [platejos, setPlatejos] = useState([]);
+    const [finalArray, setFinalArray] = useState([]);
 
-  const handleSelectItem = (ev) => {
-    setComanda(ev);
-  };
+    const handleSelectItem = (ev) => {
+      setComanda(ev);
+    };
 
-  //Fetch de pdidos al servidor
-  useEffect(() => {
-    fetch("http://localhost:3001/api/orders")
-      .then((response) => response.json())
-      .then((data) => setCocinaDatos(data));
-  }, []);
-  console.log("cocinadatos--->", cocinaDatos)
+    //Fetch de pdidos al servidor
+    useEffect(() => {
+      fetch("http://localhost:3001/api/orders")
+        .then((response) => response.json())
+        .then((data) => setCocinaDatos(data));
+    }, []);
+    console.log("cocinadatos--->", cocinaDatos)
 
-  useEffect(()=>{
-    setNoSonTicket(cocinaDatos.filter(numeropedido => numeropedido.status < 5))
-  }, [cocinaDatos])
-  console.log("no son ticket--->", noSonTicket)
-
-useEffect(()=>{
-  setPedidosArray([]);
-}, [])
-
-//
-useEffect(()=>{
-  for (let pedido of noSonTicket){
-    fetch(`http://localhost:3001/api/orders/${pedido.id}`)
-    .then((response) => response.json())
-    .then((data) => pedidosArray.push(data))//setPedidosArray([...pedidosArray, data]));
-}}, [noSonTicket, pedidosArray],)
-console.log("Pedidos Array--->", pedidosArray)
-
-
-console.log("Pedidoarray articles--->", pedidosArray.articles)
-
-useEffect(()=>{
-  setPlatejos([]);
-}, [])
-
-  useEffect(()=> {
-    for (let index of pedidosArray){
-      let platoAMeter = index.articles
-      platejos.push(platoAMeter)
-      console.log("index.articles--->", platoAMeter)
-     }
-  },[pedidosArray, platejos])
-  console.log("PLATEJOS--->", platejos)
+    useEffect(()=>{
+      setNoSonTicket(cocinaDatos.filter(numeropedido => numeropedido.status < 5))
+    }, [cocinaDatos])
+    console.log("no son ticket--->", noSonTicket)
 
   useEffect(()=>{
-    setFinalArray([]);
+    setPedidosArray([]);
   }, [])
 
-  useEffect(()=> {
-    for (let index of platejos){
-      console.log("INDEXXXXXXXX->", index)
-      for (let ind of index){
-        finalArray.push(ind.orderArticle)
-      }
-     }
-  },[])
+  //
+  useEffect(()=>{
+    for (let pedido of noSonTicket){
+      fetch(`http://localhost:3001/api/orders/${pedido.id}`)
+      .then((response) => response.json())
+      .then((data) => pedidosArray.push(data))//setPedidosArray([...pedidosArray, data]));
+  }}, [noSonTicket, pedidosArray],)
+  console.log("Pedidos Array--->", pedidosArray)
+
+  console.log("Pedidoarray articles--->", pedidosArray.articles)
+
+  useEffect(()=>{
+    setPlatejos([]);
+  }, [])
+
+    useEffect(()=> {
+      for (let index of pedidosArray){
+        let platoAMeter = index.articles
+        platejos.push(platoAMeter)
+        console.log("index.articles--->", platoAMeter)
+       }
+    },[pedidosArray, platejos])
+    console.log("PLATEJOS--->", platejos)
+
+    useEffect(()=>{
+      setFinalArray([]);
+    }, [])
+
+    useEffect(()=> {
+      for (let index of platejos){
+        console.log("INDEXXXXXXXX->", index)
+        for (let ind of index){
+          finalArray.push(ind.orderArticle)
+        }
+       }
+    },[])
+
+  
+  /* Inicio control de carga */
+
+  // const [datosArray, setDatosArray] = useState([]);
+  // const [articuloListos, setArticuloListos] = useState([]);
+  // const [datosCargados, setDatosCargados] = useState(false);
+
+  // const { id } = useParams();
+
+  // useEffect(() => {
+  //   fetch(`http://localhost:3001/api/orders/`)
+  //     .then((response) => response.json())
+  //     .then((data) => setDatosArray(data))
+  //     .catch((err) => alert(err));
+  // }, [id]);
+
+  // console.log(datosArray);
+
+  // let count = 0;
+  // // let datosCargados = false;
+  // useEffect(() => {
+  //   const id = setInterval(function log() {
+  //     count++;
+  //     console.log(`Count es: ${count}`);
+
+  //     if (datosArray !== "undefinec") {
+  //       console.log("datoscargados");
+  //       setDatosCargados(true);
+  //       setArticuloListos(datosArray.articles);
+  //       clearInterval(id);
+  //     }
+  //   }, 1000);
+
+  //   //Esta es la función de limpieza
+  //   return () => {
+  //     clearInterval(id);
+  //   };
+  // }, [count]);
 
 
+  /* fin control de carga */
 
   useEffect(() => {
-    setPedidosCocina( 
-      finalArray.filter((plato) => plato.status === 1)
-    );
-    setElaboracionCocina(
-      finalArray.filter((plato) => plato.status === 2)
-    );
-    setTerminadosCocina(
-      finalArray.filter((plato) => plato.status === 3)
-    );
-    setServidosCocina(
-      finalArray.filter((plato) => plato.status === 4)
-    );
-  },[]);
+    setPedidosCocina(finalArray.filter((plato) => plato.status === 1));
+    setElaboracionCocina(finalArray.filter((plato) => plato.status === 2));
+    setTerminadosCocina(finalArray.filter((plato) => plato.status === 3));
+    setServidosCocina(finalArray.filter((plato) => plato.status === 4));
+  }, []);
 
-console.log("PEDIDOS COCINAAAAAAAAAAAA-->", pedidosCocina)
+  console.log("PEDIDOS COCINAAAAAAAAAAAA-->", pedidosCocina);
 
   return (
-    <div className="mainDiv">
-      <div className="sectionParagraph_cocina">
-        <div className="titleAdmin_cocina">
-          <h1 className="title">
-            <Link to="/admmain">
-              <img
-                src={require("../../../images/icons/ico_flechaizquierda.png")}
-                alt="Volver al menú de administración"
-                className="arrowIcon"
-              />
-            </Link>
-            Cocina
-          </h1>
-        </div>
-
-        <div className="detallePedidos">
-          <div className="estadoItem">
-            <h1>Pedidos ({pedidosCocina.length})</h1>
-            {pedidosCocina.map((pedidos) => (
-              <div
-                className="estadoItemDiv__card"
-                key={pedidos.id}
-                // onClick={() => handleSelectItem(pedidos.id)}
-              >
-                <KitchenItem
-                  ordenId={pedidos}
-                  key={pedidos.id}
-                  style={"kitchenItemDiv"}
-                  type={"pedidos"}
-                />
-              </div>
-            ))}
-          </div>
-
-          <div className="estadoItem">
-            <h1>Elaboración ({elaboracionCocina.length})</h1>
-            {elaboracionCocina.map((pedidos) => (
-              <div
-                className="estadoItemDiv__card"
-                key={pedidos.id}
-                // onClick={() => handleSelectItem(pedidos.id)}
-              >
-                <KitchenItem
-                  ordenId={pedidos}
-                  key={pedidos.id}
-                  style={"kitchenItemDiv red"}
-                  type={"elaboracion"}
-                />
-              </div>
-            ))}
-          </div>
-
-          <div className="estadoItem">
-            <h1>Terminados ({terminadosCocina.length})</h1>
-            {terminadosCocina.map((pedidos) => (
-              <div
-                className="estadoItemDiv__card"
-                key={pedidos.id}
-                // onClick={() => handleSelectItem(pedidos.id)}
-              >
-                <KitchenItem
-                  ordenId={pedidos}
-                  key={pedidos.id}
-                  style={"kitchenItemDiv green"}
-                  type={"terminado"}
-                />
-              </div>
-            ))}
-          </div>
-
-          <div className="kitchenDetailItem__card">
-          <div className="kitchenDetailItem__card">
-            <div >
-              <h1>Servidos</h1>
-                <div className="kitchenDetailItem__card__azul"><h1 className="textH1">{servidoCocina.length}</h1></div>
-
+    <>
+      {/* {datosCargados ? ( */}
+        <div className="mainDiv">
+          <div className="sectionParagraph_cocina">
+            <div className="titleAdmin_cocina">
+              <h1 className="title">
+                <Link to="/admmain">
+                  <img
+                    src={require("../../../images/icons/ico_flechaizquierda.png")}
+                    alt="Volver al menú de administración"
+                    className="arrowIcon"
+                  />
+                </Link>
+                Cocina
+              </h1>
             </div>
 
-            <div >
-              <h1>Ticket</h1>
-              <div className="kitchenDetailItem__card__naranja"><h1 className="textH1">{ticketCocina.length}</h1></div>
+            <div className="detallePedidos">
+              <div className="estadoItem">
+                <h1>Pedidos ({pedidosCocina.length})</h1>
+                {pedidosCocina.map((pedidos) => (
+                  <div
+                    className="estadoItemDiv__card"
+                    key={pedidos.id}
+                    // onClick={() => handleSelectItem(pedidos.id)}
+                  >
+                    <KitchenItem
+                      ordenId={pedidos}
+                      key={pedidos.id}
+                      style={"kitchenItemDiv"}
+                      type={"pedidos"}
+                    />
+                  </div>
+                ))}
+              </div>
 
+              <div className="estadoItem">
+                <h1>Elaboración ({elaboracionCocina.length})</h1>
+                {elaboracionCocina.map((pedidos) => (
+                  <div
+                    className="estadoItemDiv__card"
+                    key={pedidos.id}
+                    // onClick={() => handleSelectItem(pedidos.id)}
+                  >
+                    <KitchenItem
+                      ordenId={pedidos}
+                      key={pedidos.id}
+                      style={"kitchenItemDiv red"}
+                      type={"elaboracion"}
+                    />
+                  </div>
+                ))}
+              </div>
+
+              <div className="estadoItem">
+                <h1>Terminados ({terminadosCocina.length})</h1>
+                {terminadosCocina.map((pedidos) => (
+                  <div
+                    className="estadoItemDiv__card"
+                    key={pedidos.id}
+                    // onClick={() => handleSelectItem(pedidos.id)}
+                  >
+                    <KitchenItem
+                      ordenId={pedidos}
+                      key={pedidos.id}
+                      style={"kitchenItemDiv green"}
+                      type={"terminado"}
+                    />
+                  </div>
+                ))}
+              </div>
+
+              <div className="kitchenDetailItem__card">
+                <div className="kitchenDetailItem__card">
+                  <div>
+                    <h1>Servidos</h1>
+                    <div className="kitchenDetailItem__card__azul">
+                      <h1 className="textH1">{servidoCocina.length}</h1>
+                    </div>
+                  </div>
+
+                  <div>
+                    <h1>Ticket</h1>
+                    <div className="kitchenDetailItem__card__naranja">
+                      <h1 className="textH1">{ticketCocina.length}</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-
-          </div>
-
           </div>
         </div>
-      </div>
-    </div>
+      {/* ) : (
+        "Cargando datos ..."
+      )} */}
+    </>
   );
 };
 
--- src/pages/admin/DetalleTickets/DetalleTickets.jsx
+++ src/pages/admin/DetalleTickets/DetalleTickets.jsx
@@ -1,102 +1,148 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const Tickets = () => {
-
   const [datosArray, setDatosArray] = useState([]);
+  const [articuloListos, setArticuloListos] = useState([]);
+  const [datosCargados, setDatosCargados] = useState(false)
 
+  const { id } = useParams();
 
-  const {id} = useParams();
+  useEffect(() => {
+    fetch(`http://localhost:3001/api/orders/${id}`)
+      .then((response) => response.json())
+      .then((data) => setDatosArray(data))
+      .catch((err) => alert(err));
+  }, [id]);
+
+  console.log(datosArray);
+
+  let count = 0;
+  // let datosCargados = false;
+  useEffect(() => {
+    const id = setInterval(function log() {
+      count++;
+      console.log(`Count es: ${count}`);
+
+      if (datosArray !== "undefinec") {
+        console.log("datoscargados");
+        setDatosCargados(true);
+        setArticuloListos(datosArray.articles);
+        clearInterval(id);
+      }
+    }, 1000);
+
+    //Esta es la función de limpieza
+    return () => {
+      clearInterval(id);
+    };
+  }, [count]);
+
+  // let postOrder = async () => {
+  //   try {
+  //     let response = await fetch(`http://localhost:3001/api/orders/22`);
+
+  //     let responseJson = await response.json();
+
+  //     if (response.status === 200){
+  //       console.log("Buena")
+  //       console.log(responseJson)
+  //       setDatosArray(responseJson)
+  //     }
+
+  // } catch (err) {
+  //   console.log(err);
+  // }}
 
   // useEffect(() => {
-  //   fetch(`http://localhost:3001/api/orders/22`)
-  //     .then((response) => response.json())
-  //     .then((data) => setDatosArray(data))
-  //     .catch(err => alert(err))
-  // }, [id]);
 
+  //   postOrder()
+  // }, []);
 
-  // console.log(datosArray);
+  // let articuloListos = [];
+  // if (datosArray.articles !== 'undefined' ){
+  //   articuloListos = datosArray.articles
+  // }
 
-
-  let postOrder = async () => {
-    try {
-      let response = await fetch(`http://localhost:3001/api/orders/22`);
-
-      let responseJson = await response.json();
-
-      if (response.status === 200){
-        console.log("Buena")
-        console.log(responseJson)
-        setDatosArray(responseJson)
-      }
-    
-  } catch (err) {
-    console.log(err);
-  }}
-  
-  useEffect(() => {
-
-    postOrder()
-  }, []);
-
-  
-let articuloListos = [];
-if (datosArray.articles !== 'undefined' ){
-  articuloListos = datosArray.articles
-}
-
- //
-
+  //
 
   return (
     <div className="mainDiv">
       <div className="sectionParagraph">
-      <div className="titleAdmin">
-            <h1 className="title">
+        <div className="titleAdmin">
+          <div>
+          <h1 className="title">
             <Link to="/listadotickets">
-            <img src={require("../../../images/icons/ico_flechaizquierda.png")} alt="Volver al listado de tickets" className="arrowIcon"/>
+              <img
+                src={require("../../../images/icons/ico_flechaizquierda.png")}
+                alt="Volver al listado de tickets"
+                className="arrowIcon"
+              />
             </Link>
-            Ticket {id} / {datosArray.userId}</h1>
-      </div>
-        <table className="rwd-table">
-          <thead>
-            <tr>
-              <th>Linea</th>
-              <th>Producto</th>
-              <th>Importe</th>
-              <th>IVA</th>
-              <th>PVP</th>
-            </tr>
-          </thead>
-          <tbody>
+            Ticket
+            </h1>
+            </div>
+          </div>
+          <div>
+            <div>
+            <h3>
+            Número: {datosArray.id} / 
+            Comensales: {datosArray.peopleCount} / 
+            Importe: {datosArray.costNeto} / 
+            IVA: {datosArray.iva} / 
+            PVP: {datosArray.pvp} / 
+            Estado: {datosArray.status}
+            <br />
+            {/* Cliente: {datosArray.user.firstName} {datosArray.user.lastName} / 
+            Teléfono: {datosArray.user.phone} / 
+            Email: {datosArray.user.email} */}
 
-          
+            </h3>
+            </div>
 
-            {articuloListos.map((item) => (
-              <tr key={item.id}>
-                <td data-th="Ticket:" className="div__table__center"><span>{item.id}</span></td>
-                <td data-th="Mesa:" className="div__table__center"><span>{item.name}</span></td>
-                <td data-th="Importe:" className="div__table__right"><span>{item.costNeto}</span></td>
-                <td data-th="IVA:" className="div__table__right"><span>{item.iva}</span></td>
-                <td data-th="P.V.P.:" className="div__table__right"><span>{item.pvp}</span></td>
-              </tr>
-            ))}
+{/* 
+
+        "user": {
+
+        "userName": "daB",
+        "firstName": "David",
+        "lastName": "Balboa",
+        "phone": "65812965",
+        "email": "daB@gmail.com",
+ */}
 
 
-            {datosArray.articles.map((item) => (
-              <tr key={item.id}>
-                <td data-th="Ticket:" className="div__table__center"><span>{item.id}</span></td>
-                <td data-th="Mesa:" className="div__table__center"><span>{item.name}</span></td>
-                <td data-th="Importe:" className="div__table__right"><span>{item.costNeto}</span></td>
-                <td data-th="IVA:" className="div__table__right"><span>{item.iva}</span></td>
-                <td data-th="P.V.P.:" className="div__table__right"><span>{item.pvp}</span></td>
-              </tr>
-            ))}
+         
+        </div>
 
-          </tbody>
-        </table>
+        {datosCargados ? (
+          <>
+            <table className="rwd-table">
+              <thead>
+                <tr>
+                  <th>Linea</th>
+                  <th>Producto</th>
+                  <th>Importe</th>
+                  <th>IVA</th>
+                  <th>PVP</th>
+                </tr>
+              </thead>
+              <tbody>
+                {datosArray.articles.map((item) => (
+                  <tr key={item.id}><td data-th="Ticket:" className="div__table__center"><span>{item.id}</span></td>
+                    <td data-th="Mesa:" className="div__table__center"><span>{item.name}</span></td>
+                    <td data-th="Importe:" className="div__table__right"><span>{item.costNeto}</span></td>
+                    <td data-th="IVA:" className="div__table__right"><span>{item.iva}</span></td>
+                    <td data-th="P.V.P.:" className="div__table__right"><span>{item.pvp}</span></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        ) : (
+          "Cargando datos ..."
+        )}
       </div>
     </div>
   );
